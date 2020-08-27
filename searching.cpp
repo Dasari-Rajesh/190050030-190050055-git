95114f52c843a2d03cb9ca55578bf6d3a026826e
Sonnandh Kalaka <190050055@iitb.ac.in>
2020-08-27 21:56:00 +0530

    change implementation to binary-search

--- searching.cpp
+++ searching.cpp
@@ -1,14 +1,10 @@
 #include"searching.h"
 using namespace std;
-bool search_custom(vector<int> v ,int x){
-	//input guaranteed to be in sorted order 
-	bool f =0;
-	for( size_t i =0 ; i<v.size() ; i++ ){
-		if( x==v[i]){
-			f =1;
-			break;
-		}
-	}
-	return f;
 
+bool search_custom(vector<int> v ,int x,int l,int r){
+	if(x == v[(l+r)/2])return true;
+	if(l == r)return false;
+	if(x > v[(l+r)/2])return search_custom(v,x,(l+r)/2 + 1,r);
+	return search_custom(v,x,l,(l+r)/2);
 }
+
